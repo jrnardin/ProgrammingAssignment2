280ff6d3c5147d9cdff821ed20d0bece8bf7d1e4
jrnardin <organization_freak@yahoo.com>
2017-08-05 12:11:33 -0700

    JRN version of Assignment #2
    Includes my changes to the functions (so they work) and comments
    describing the functions & file, in general

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment #2 in Week #3 of the R Programming Course
+## this assignment is to write a set of functions that enable caching of 
+## data to reduce the repetition of potentially time-consuming  computations
 
-## Write a short comment describing this function
+## personally, I like to put my comments about a function, within the function 
+## itself, so I have done that below with just single # instead of double
 
 makeCacheMatrix <- function(x = matrix()) {
+      # builds a set of functions and returns the functions within 
+      # a list to the parent environment
+            
+            m <- NULL
+            set <- function(y) {
+                  x <<- y
+                  m <<- NULL
+            }
+            get <- function() x
+            setSolve <- function(solve) m <<- solve
+            getSolve <- function() m
+            list(set = set, get = get,
+                 setSolve = setSolve,
+                 getSolve = getSolve)
+      }
 
-}
 
-
-## Write a short comment describing this function
+## still prefer to put comments inside the function being described
+## is that lexical commenting?  ;)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      #requires an argument that is returned by makeCachMatrix() 
+      #function then retrieves the inverse of that matrix from the cached value 
+      #that is stored in the makeCacheMatrix() object's environment.
+      
+      m <- x$getSolve()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setSolve(m)
+      m
 }
